c4017436c5b5f461b965a5eac986abbf6049a5f5
Daniel Hildebrandt <48860201+dnhldbrndt@users.noreply.github.com>
2024-07-02 17:09:13 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,12 @@
 <?php
 //Config.php 
 	return array(
-	
 		'comic_path' 		=> 		'imgpost',		 
 		'image_suffix' 		=> 		'.jpg',			 
-		'update_interval'   => 		'86400',		  
-		'news_path'			=> 		'txtpost',
+		'update_interval'   	=> 		'86400',		  
+		'news_path'		=> 		'txtpost',
 		'news_suffix'		=>		'.php',
-		'first_strip'		=> 		'2017-12-30'
-			
+		'first_strip'		=> 		'2017-12-30'	
 	);
 ?>
 
