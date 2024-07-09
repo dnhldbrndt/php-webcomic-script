1f767c743138e30bb93227601922a70214e2e6e5
Daniel Hildebrandt <48860201+dnhldbrndt@users.noreply.github.com>
2024-07-09 16:53:38 +0200

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,35 +1,31 @@
 <div id="nav">
-	<div>
-		<a href="index.php?date=
-			<?php 
-			$configs = include('config.php'); 
-			$first_strip = $configs['first_strip']; 
-			echo $first_strip;
-			?>
-		">First</a>
-	</div>
     <div>
-        <?php 
-			if ($previous) { 
-				echo '<a href="index.php?date=' . $previous . '"> Previous </a>';
-			} 
-			else { 
-				echo 'Previous';
-			}
-		?>
-	</div>
-	<div>
-		<?php
-			$next = $this->imagepost->getNext($date);
-			if ($next) {
-				echo '<a href="index.php?date=' . $next . '">Next</a>'; 
-            } 
-			else {
-				echo 'Next'; 
-			}
-		?>
+        <a href="index.php?date=<?php 
+            $configs = include('config.php'); 
+            $first_strip = $configs['first_strip']; 
+            echo $first_strip; 
+        ?>">First</a>
     </div>
     <div>
-		<a href="index.php">Latest</a>
- </div>
- </div>
+        <?php 
+            if ($previous) { 
+                echo '<a href="index.php?date=' . $previous . '">Previous</a>'; 
+            } else { 
+                echo 'Previous'; 
+            }
+        ?>
+    </div>
+    <div>
+        <?php
+            $next = $this->imagepost->getNext($date);
+            if ($next) {
+                echo '<a href="index.php?date=' . $next . '">Next</a>'; 
+            } else {
+                echo 'Next'; 
+            }
+        ?>
+    </div>
+    <div>
+        <a href="index.php">Latest</a>
+    </div>
+</div>
